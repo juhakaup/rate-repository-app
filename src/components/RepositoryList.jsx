3a3fcefaa73cd301492fa699fca82178078abb83
Juha Kauppinen <scytile@gmail.com>
2023-08-18 21:57:07 +0300

    ex. 10.25 - filter repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,9 +1,9 @@
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
-import Text from './Text';
-import { Button, Menu, PaperProvider } from 'react-native-paper';
-import { useState } from 'react';
+import { Button, Menu, PaperProvider, TextInput } from 'react-native-paper';
+import React, { useState } from 'react';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
@@ -14,18 +14,25 @@ const styles = StyleSheet.create({
 
 export const ItemSeparator = () => <View style={styles.separator} />;
 
-const FilterSelector = ({ sorting, setSorting }) => {
+const SearchField = ({ searchKeyword, setSearchKeyword }) => {
+  return (
+    <TextInput 
+    label="Search term"
+    value={searchKeyword}
+    onChangeText={text => setSearchKeyword(text)}
+    />
+  )
+}
+
+const FilterSelector = ({ orderBy, setOrderBy, orderDirection, setOrderDirection }) => {
   const [visible, setVisible] = useState(false);
-
   const openMenu = () => setVisible(true);
-
   const closeMenu = () => setVisible(false);
-
   let menuText;
 
-  if (sorting.orderBy == "CREATED_AT") {
+  if (orderBy == "CREATED_AT") {
     menuText = "Latest repositories";
-  } else if (sorting.orderBy === "RATING_AVERAGE" && sorting.orderDirection === "DESC") {
+  } else if (orderBy === "RATING_AVERAGE" && orderDirection === "DESC") {
     menuText = "Higest rated repositories";
   } else {
     menuText = "Lowest rated repositories";
@@ -45,74 +52,81 @@ const FilterSelector = ({ sorting, setSorting }) => {
           anchor={<Button onPress={openMenu} icon="chevron-down" contentStyle={{flexDirection: 'row-reverse'}}>{menuText}</Button>}>
           <Menu.Item title="Select an item..." disabled/>
           <Menu.Item onPress={() => {
-            setSorting(
-              {
-                "orderBy": "CREATED_AT",
-                "orderDirection": "DESC"
-              }
-            )
+            setOrderBy("CREATED_AT");
+            setOrderDirection("DESC");
           }} title="Latest repositories" />
             
           <Menu.Item onPress={() => {
-            setSorting(
-              {
-                "orderBy": "RATING_AVERAGE",
-                "orderDirection": "DESC"
-              }
-            )
+            setOrderBy("RATING_AVERAGE");
+            setOrderDirection("DESC");
           }} title="Higest rated repositories" />
 
           <Menu.Item onPress={() => {
-            setSorting(
-              {
-                "orderBy": "RATING_AVERAGE",
-                "orderDirection": "ASC"
-              }
-            )
+            setOrderBy("RATING_AVERAGE");
+            setOrderDirection("ASC");
           }} title="Lowest rated repositories" />
         </Menu>
       </View>
   )
 }
 
-export const RepositoryListContainer = ({ repositories, sortBy, setSorting }) => {
-  const repositoryNodes = repositories
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    const props = this.props;
+    const { searchKeyword, setSearchKeyword, setOrderBy, orderBy, setOrderDirection, orderDirection } = props;
+    return (
+      <>
+        <SearchField searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword}/>
+        <FilterSelector 
+          setOrderBy={setOrderBy}
+          orderBy={orderBy}
+          setOrderDirection={setOrderDirection}
+          orderDirection={orderDirection}
+          setSearchKeyword={setSearchKeyword}
+        />
+      </>
+    );
+  }
+
+  render() {
+    const props = this.props;
+    const repositories = props.repositories;
+    const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
 
-  return (
-    <PaperProvider>
+    return(
+      <PaperProvider>
       <FlatList
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={ItemSeparator}
         data={repositoryNodes}
-        renderItem={({item}) => (
-          <RepositoryItem item={item} singleItem={false}/>
-        )}
-        ListHeaderComponent={() => <FilterSelector sorting={sortBy} setSorting={setSorting} />}
+        renderItem={({item}) => (<RepositoryItem item={item} singleItem={false}/>)}
+        ListHeaderComponent={this.renderHeader}
       />
     </PaperProvider>
-  );
+    )
+  }
 }
 
 const RepositoryList = () => {
-  const [sortBy, setSortBy] = useState({
-    "orderBy": "CREATED_AT",
-    "orderDirection": "DESC"
-  });
-  
-  const setSorting = (item) => {
-    setSortBy(item)
-  }
+  const [orderBy, setOrderBy] = useState("CREATED_AT");
+  const [orderDirection, setOrderDirection] = useState("DESC");
+  const [searchKeyword, setSearchKeyword] = useState("");
 
-  const{ repositories, loading } = useRepositories(sortBy);
-
-  if (loading) {
-    return (<Text>Loading...</Text>)
-  }
+  const [value] = useDebounce(searchKeyword, 500);
+  const{ repositories } = useRepositories({ orderBy, orderDirection, searchKeyword: value });
 
   return ( 
-    <RepositoryListContainer repositories={repositories} sortBy={sortBy} setSorting={setSorting} />
+    <RepositoryListContainer 
+      repositories={repositories} 
+      setOrderBy={setOrderBy}
+      orderBy={orderBy}
+      setOrderDirection={setOrderDirection}
+      orderDirection={orderDirection}
+      setSearchKeyword={setSearchKeyword}
+      searchKeyword={searchKeyword}
+    />
   )
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,13 +1,11 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = ( sorting ) => {
-
-  console.log("use repo", sorting)
+const useRepositories = ( params ) => {
 
   const result = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
-    variables: sorting
+    variables: params
   });
 
   return { 
